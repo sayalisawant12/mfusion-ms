d63925987c583174919e03c1e75e91548b00bf15
sayalisawant12 <sayalisawant1275@gmail.com>
2024-12-04 20:35:55 +0530

    adding jenkinsfile

---
+++ src/main/java/com/example/mfusion_ms/MfusionMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mfusion_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfusionMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfusionMsApplication.class, args);
+	}
+
+}
